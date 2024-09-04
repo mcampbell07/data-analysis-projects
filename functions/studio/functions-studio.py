09fd70a01b3c40815a4c001dfdcb4da737bedcf1
mcampbell07/usr/local/bin/python3 <megancampbellrdn@gmail.com>
2024-09-04 18:17:31 -0500

    studio updates

--- functions/studio/functions-studio.py
+++ functions/studio/functions-studio.py
@@ -1,18 +1,56 @@
-# We want to COMPLETELY reverse a list by flipping the order of the entries AND flipping the order of characters in each element.
+# # We want to COMPLETELY reverse a list by flipping the order of the entries AND flipping the order of characters in each element.
 
-# a) Define a 'reverse_characters' function. Give it one parameter, which will be the string to reverse.
-# b) Within the function, use the 'list' function to split a string into a list of individual characters
-# c) 'reverse' your new list.
-# d) Use 'join' to create the reversed string and return that string from the function.
-# e) Create a variable of type string to test your new function. # f) Use 'print(reverse_characters(my_variable_name))'; to call the function and verify that it correctly reverses the characters in the string.
+# # a) Define a 'reverse_characters' function. Give it one parameter, which will be the string to reverse.
+# def reverse_characters(string_input): #define reverse_characters function with the parameter string_input
+   
+
+# # b) Within the function, use the 'list' function to split a string into a list of individual characters
+# def reverse_characters(string_input):
+#     string_list = list(string_input)
+#     return string_list
+
+# # c) 'reverse' your new list.
+# def reverse_characters(string_input):
+#     string_list = list(string_input)
+#     string_list.reverse()
+#     return string_list
+
+
+# # d) Use 'join' to create the reversed string and return that string from the function.
+# def reverse_characters(string_input):
+#     string_list = list(string_input)
+#     string_list.reverse()
+#     reversed_string ="".join(string_list)
+#     return reversed_string
+
+# # e) Create a variable of type string to test your new function. 
+# "hello"
+
+# # # f) Use 'print(reverse_characters(my_variable_name))'; to call the function and verify that it correctly reverses the characters in the string.
+
+# print(reverse_characters("hello"))
+
+
 # g) Use method chaining to reduce the lines of code within the function.
 
 
 
 # 2) The 'split' method does not work on numbers, but we want the function to return a number with all the digits reversed (e.g. 1234 converts to 4321 and NOT the string "4321")
+
 # a) Add an if statement to your reverse_characters function to check the typeof the parameter.
+def reverse_characters(string_input):
+    string_list = list(string_input)
+    string_list.reverse()
+    reversed_string ="".join(string_list)
+    if type(string_input) == 'string':
+        return reversed_string
+
+print(reverse_characters('hello'))
 # b - d) If type is ‘string’, return the reversed string as before. If type is ‘number’, convert the parameter to a string, reverse the characters, then convert it back into a number. Return the reversed number.
+
+
 # e) Be sure to print the result returned by the function to verify that your code works for both strings and numbers. Do this before moving on to the next steps.
+
 
 # 3) Create a new function with one parameter, which is the list we want to change. The function should:
 # a) Define and initialize an empty list.
